de38e8b948211ecfd6072508fae2dd9a0a8c3b41
Anand Chowdhary <mail@anandchowdhary.com>
2020-07-07 20:12:21 +0530

    :sparkles: Use underscores in object keys

--- index.mjs
+++ index.mjs
@@ -1,9 +1,12 @@
 import polka from "polka";
-import dot from "dot-object";
+import Dot from "dot-object";
 import UAParser from "ua-parser-js";
 import geolite2 from "geolite2-redist";
+import anonymize from "ip-anonymize";
 import maxmind from "maxmind";
 import fs from "fs";
+
+const dot = new Dot("_");
 
 const lookup = geolite2.open("GeoLite2-City", (path) => {
   let lookupBuffer = fs.readFileSync(path);
@@ -16,35 +19,38 @@ polka()
     const userAgent = (req.headers["user-agent"] || "").substring(0, 1000);
     const userAgentParser = new UAParser(userAgent);
     const userAgentResult = {
-      userAgent,
+      string: userAgent,
       browser: userAgentParser.getBrowser(),
       cpu: userAgentParser.getCPU(),
       device: userAgentParser.getDevice(),
       engine: userAgentParser.getEngine(),
       os: userAgentParser.getOS(),
     };
-    data.userAgent = userAgentResult;
-    const ip =
+    data.user_agent = userAgentResult;
+    let ip =
       req.headers["x-forwarded-for"] ||
       req.connection.remoteAddress ||
       req.socket.remoteAddress ||
       (req.connection.socket ? req.connection.socket.remoteAddress : null);
-    const locationValue = dot.dot(
-      lookup.get(!ip || ip === "::1" ? "66.6.44.4" : ip)
+    ip = (ip === "::1" ? "66.6.44.4" : ip) || "66.6.44.4";
+    data.anonymous_ip = anonymize(ip);
+    try {
+      const locationValue = dot.dot(lookup.get(ip));
+      Object.keys(locationValue).forEach((key) => {
+        if (key.includes("_names_") && !key.includes("_names_en"))
+          delete locationValue[key];
+      });
+      data.location = locationValue;
+    } catch (error) {}
+    const saveObject = JSON.parse(
+      JSON.stringify(dot.dot(data)).replace(/\[/g, "_").replace(/\]/g, "")
     );
-    Object.keys(locationValue).forEach((key) => {
-      if (key.includes(".names.") && !key.includes(".names.en"))
-        delete locationValue[key];
-    });
-    data.location = locationValue;
-
-    const saveObject = dot.dot(data);
     Object.keys(saveObject).forEach(
       (key) =>
         (saveObject[key] === undefined || saveObject[key] === null) &&
         delete saveObject[key]
     );
-    // console.log(saveObject);
+    console.log(saveObject);
     res.end("OK");
   })
   .listen(3333, (error) => {

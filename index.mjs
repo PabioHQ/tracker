dea27d2ce4ccfdd27b5672988bd52c86ed63f8f1
Anand Chowdhary <mail@anandchowdhary.com>
2020-07-07 19:30:49 +0530

    :sparkles: Get user agent data

--- index.mjs
+++ index.mjs
@@ -1,10 +1,20 @@
 import polka from "polka";
+import UAParser from "ua-parser-js";
 
 polka()
   .all("/", (req, res) => {
-    const data = {
-      userAgent: (req.headers["user-agent"] ?? "").substring(0, 1000),
+    const data = {};
+    const userAgent = (req.headers["user-agent"] ?? "").substring(0, 1000);
+    const userAgentParser = new UAParser(userAgent);
+    const userAgentResult = {
+      userAgent,
+      browser: userAgentParser.getBrowser(),
+      cpu: userAgentParser.getCPU(),
+      device: userAgentParser.getDevice(),
+      engine: userAgentParser.getEngine(),
+      os: userAgentParser.getOS(),
     };
+    data.userAgent = userAgentResult;
     console.log(data);
     res.end("OK");
   })
